0aca8f7cc7352d60dda0e2812b39a6266e68c5f0
C Dragon <cdragon.kim@me.com>
2015-10-04 20:31:12 +0900

    핵심정리 2 시작...

---
+++ src/github.com/cdragonkim/core02/op1.go
@@ -0,0 +1,9 @@
+package core02
+
+import (
+	"fmt"
+)
+
+func Op1() {
+	fmt.Println("test op1")
+}
---
+++ src/github.com/cdragonkim/core02main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/cdragonkim/core02"
+)
+
+func main() {
+	core02.Op1()
+}
