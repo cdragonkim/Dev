ee48d73b764155e5e84b2025837b58d4f061f045
C Dragon <cdragon.kim@me.com>
2015-10-07 17:59:18 +0900

    10/7 3th

---
+++ src/github.com/cdragonkim/unit18/GotoStmt/GotoStmt.go
@@ -0,0 +1,10 @@
+package GotoStmt
+
+import (
+	"fmt"
+)
+
+func GotoStmt1() {
+	var a int = 1
+	fmt.Println(a)
+}
---
+++ src/github.com/cdragonkim/unit18/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cdragonkim/unit18/GotoStmt"
+)
+
+func main() {
+	fmt.Println("for goto 연습")
+	GotoStmt.GotoStmt1()
+	fmt.Println("--------------")
+}
