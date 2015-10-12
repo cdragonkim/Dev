2e5b2f977756b6902069ebe0b9f7de7fd42b082e
C Dragon <cdragon.kim@me.com>
2015-10-12 21:37:18 +0900

    map add

---
+++ src/github.com/cdragonkim/unit23/mapPractices/mapDeletePractice.go
@@ -0,0 +1,15 @@
+package mapPractices
+
+import (
+	"fmt"
+)
+
+func MapPractice1() {
+	var a map[string]int = make(map[string]int)
+	var b = make(map[string]int)
+	c := make(map[string]int)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
