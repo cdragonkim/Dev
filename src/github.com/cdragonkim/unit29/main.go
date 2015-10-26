27ad929cbd94766c9c6ff2eb52958bf7577310bd
C Dragon <cdragon.kim@me.com>
2015-10-26 17:20:39 +0900

    10/26 1st

--- src/github.com/cdragonkim/unit29/main.go
+++ src/github.com/cdragonkim/unit29/main.go
@@ -9,4 +9,31 @@ func main() {
 	fmt.Println("-- struct Practice 1 --")
 	structPractices.StructPractice1()
 	fmt.Println("***********************")
+
+	fmt.Println("-- struct Practice 2 --")
+	structPractices.StructPractice2()
+	fmt.Println("***********************")
+
+	fmt.Println("-- struct Practice 3 --")
+	fmt.Println("-- 구조체 매개변수 사용하기 --")
+	structPractices.StructPractice3()
+	fmt.Println("***********************")
+
+	fmt.Println("-- struct Practice 4 --")
+	fmt.Println("-- 구조체 매개변수 사용하기 2 --")
+	structPractices.StructPractice4()
+	fmt.Println("***********************")
+
+	fmt.Println("-- struct Practice 5 --")
+	fmt.Println("-- 구조체에 메소드 연결 --")
+	structPractices.StructPractice5()
+	fmt.Println("***********************")
+
+	fmt.Println("-- struct Practice 6 --")
+	structPractices.StructPractice6()
+	fmt.Println("***********************")
+
+	fmt.Println("-- struct Practice 7 --")
+	structPractices.StructPractice7()
+	fmt.Println("***********************")
 }
--- src/github.com/cdragonkim/unit29/structPractices/structPractce1.go
+++ src/github.com/cdragonkim/unit29/structPractices/structPractce1.go
@@ -21,3 +21,86 @@ func StructPractice1() {
 	fmt.Println(rect2)
 	fmt.Println(rect3)
 }
+func NewRectangle(width, height int) *Rectangle {
+	return &Rectangle{width, height}
+}
+
+func StructPractice2() {
+	rect := NewRectangle(20, 10)
+	fmt.Println(rect)
+}
+
+func rectangleArea(rect *Rectangle) int {
+	return rect.width * rect.height
+}
+
+func StructPractice3() {
+	rect := Rectangle{30, 30}
+	area := rectangleArea(&rect)
+
+	fmt.Println(area)
+}
+
+func rectangleScaleA(rect *Rectangle, factor int) {
+	rect.width = rect.width * factor
+	rect.height = rect.height * factor
+}
+
+func rectangleScaleB(rect Rectangle, factor int) {
+	rect.width = rect.width * factor
+	rect.height = rect.height * factor
+}
+
+func StructPractice4() {
+	rect1 := Rectangle{30, 30}
+	rectangleScaleA(&rect1, 10)
+	fmt.Println(rect1)
+
+	rect2 := Rectangle{30, 30}
+	rectangleScaleB(rect2, 10)
+	fmt.Println(rect2)
+}
+
+//----------------------------------------------
+
+func (rect *Rectangle) area() int {
+	return rect.width * rect.height
+}
+
+func StructPractice5() {
+	rect := Rectangle{10, 20}
+	fmt.Println(rect.area())
+}
+
+//----------------------------------------------
+
+func (rect *Rectangle) scaleA(factor int) {
+	rect.width = rect.width * factor
+	rect.height = rect.height * factor
+}
+
+func (rect Rectangle) scaleB(factor int) {
+	rect.width = rect.width * factor
+	rect.height = rect.height * factor
+}
+
+func StructPractice6() {
+	rect1 := Rectangle{30, 30}
+	rect1.scaleA(10)
+	fmt.Println(rect1)
+
+	rect2 := Rectangle{30, 30}
+	rect2.scaleB(10)
+	fmt.Println(rect2)
+}
+
+//----------------------------------------------
+
+func (_ Rectangle) dummy() {
+	fmt.Println("dummy")
+}
+
+func StructPractice7() {
+	var r Rectangle
+	r.dummy()
+}
