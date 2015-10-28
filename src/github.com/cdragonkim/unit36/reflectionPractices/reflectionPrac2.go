ab0bc172102a8dc06b34828e2a44855eec91787b
C Dragon <cdagon.kim@me.com>
2015-10-28 23:18:02 +0900

    unit36 add 2

--- src/github.com/cdragonkim/unit36/reflectionPractices/reflectionPrac2.go
+++ src/github.com/cdragonkim/unit36/reflectionPractices/reflectionPrac2.go
@@ -15,4 +15,7 @@ func ReflectionPrac21() {
 	p := Person{}
 	name, ok := reflect.TypeOf(p).FieldByName("name")
 	fmt.Println(ok, name.Tag.Get("tag1"), name.Tag.Get("tag2"))
+
+	age, ok := reflect.TypeOf(p).FieldByName("age")
+	fmt.Println(ok, age.Tag.Get("tag1"), age.Tag.Get("tag2"))
 }
