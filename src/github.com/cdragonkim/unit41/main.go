893559e26991651c348ef17dc75f8c97ace2ab1f
C Dragon <cdragon.kim@me.com>
2015-11-03 17:19:29 +0900

    unit41 - print func -  start

---
+++ src/github.com/cdragonkim/unit41/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cdragonkim/unit41/printFuncPractices"
+)
+
+func main() {
+	fmt.Println("-- 출력 함수 테스트 1 --")
+	printFuncPractices.PrintFuncPrac11()
+	fmt.Println("***********************")
+}
---
+++ src/github.com/cdragonkim/unit41/printFuncPractices/printFuncPrac1.go
@@ -0,0 +1,30 @@
+package printFuncPractices
+
+import (
+	"fmt"
+)
+
+func PrintFuncPrac11() {
+	var num1 int = 10
+	var num2 float32 = 3.2
+	var num3 complex64 = 2.5 + 8.1i
+	var s string = "Hello, world!"
+	var b bool = true
+	var a []int = []int{1, 2, 3}
+	var m map[string]int = map[string]int{"Hello": 1}
+	var p *int = new(int)
+	type Data struct{ a, b int }
+	var data Data = Data{1, 2}
+	var i interface{} = 1
+
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Println(num3)
+	fmt.Println(s)
+	fmt.Println(b)
+	fmt.Println(a)
+	fmt.Println(m)
+	fmt.Println(p)
+	fmt.Println(data)
+	fmt.Println(i)
+}
